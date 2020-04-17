142fdfb44cfd869c677de8be656af0f633ce671e
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-17 00:33:06 +0000

    Expanded on Artist & Song classes

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,19 @@
 class Artist 
+  @@all = []
   
+  def self.all
+    @@all 
+  end
+  
+  attr_accessor :name
+  attr_reader :songs
+  
+  def initialize(name)
+    @name = name 
+    @songs = []
+  end
+  
+  def save
+    @@all << self
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,12 @@
 class Song 
+  @@all = []
   
+  def self.all
+    @@all 
+  end
+  
+  attr_accessor :artist, :title, :tempo, :key, :relative_minor, :lyrics
+  
+  def initialize(artist, title)
+  end
 end
