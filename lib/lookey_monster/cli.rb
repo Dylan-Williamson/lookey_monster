20c5cf25c1717264c66b0c66035de56741d0e2c2
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-18 15:10:22 +0000

    Updated CLI design

--- lib/lookey_monster/cli.rb
+++ lib/lookey_monster/cli.rb
@@ -12,7 +12,27 @@ class LookeyMonster::CLI
 ▐█ ▌▐▌▐█· ▄█▀▄ ▐█▐▐▌▄▀▀▀█▄ ▐█.▪▐▀▀▪▄▐▀▀▄ 
 ██ ██▌▐█▌▐█▌.▐▌██▐█▌▐█▄▪▐█ ▐█▌·▐█▄▄▌▐█•█▌
 ▀▀  █▪▀▀▀ ▀█▄▀▪▀▀ █▪ ▀▀▀▀  ▀▀▀  ▀▀▀ .▀  ▀\n\n\n\n".light_cyan
-    print "       Please enter artist below:\n\n\n".light_red
+    menu
+    goodbye
   end
   
+  def menu
+    print "       Please enter artist below:\n\n\n".light_red
+    input = gets.chomp
+  end
+  
+  def goodbye
+    print "\n\n\n\n
+    ▄▄▌              ▄ •▄ ▄▄▄ . ▄· ▄▌    
+    ██•  ▪     ▪     █▌▄▌▪▀▄.▀·▐█▪██▌    
+    ██▪   ▄█▀▄  ▄█▀▄ ▐▀▀▄·▐▀▀▪▄▐█▌▐█▪    
+    ▐█▌▐▌▐█▌.▐▌▐█▌.▐▌▐█.█▌▐█▄▄▌ ▐█▀·.    
+    .▀▀▀  ▀█▄▀▪ ▀█▄▀▪·▀  ▀ ▀▀▀   ▀ •     
+• ▌ ▄ ·.        ▐ ▄ .▄▄ · ▄▄▄▄▄▄▄▄ .▄▄▄  
+·██ ▐███▪▪     •█▌▐█▐█ ▀. •██  ▀▄.▀·▀▄ █·
+▐█ ▌▐▌▐█· ▄█▀▄ ▐█▐▐▌▄▀▀▀█▄ ▐█.▪▐▀▀▪▄▐▀▀▄ 
+██ ██▌▐█▌▐█▌.▐▌██▐█▌▐█▄▪▐█ ▐█▌·▐█▄▄▌▐█•█▌
+▀▀  █▪▀▀▀ ▀█▄▀▪▀▀ █▪ ▀▀▀▀  ▀▀▀  ▀▀▀ .▀  ▀".light_cyan
+    print "\n\n\n\n   Thank you for using LooKey Monster!\n\n\n".light_red
+  end
 end
