e5d27ff794160c6c0b0bab6ee90b37eec6672306
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-18 23:12:19 +0000

    Updates APIManager

--- lib/api_manager.rb
+++ lib/api_manager.rb
@@ -1,9 +1,14 @@
 class LookeyMonster::APIManager
-  
   BASE_URL = "https://api.getsongbpm.com"
-  APIKEY = "4a45338f3b72c92981ea4c26c94ebf61"
   def self.get_song_info
-    url = BASE_URL + "/song/"
-    res = HTTParty.get(url)
+    api_key = "4a45338f3b72c92981ea4c26c94ebf61"
+    url = BASE_URL + "/song"
+    headers = {"X-API-KEY" => api_key}
+    query = {"api_key" => api_key} 
+    res = HTTParty.get(url, query: query)
   end
 end
+# apikey = 4a45338f3b72c92981ea4c26c94ebf61
+# Web API Base URL	https://api.getsongbpm.com
+# Method	GET
+# Authorization	A valid API Key must be sent with all client requests, either via URL_PARAM (api_key), or a X-API-KEY header parameter
