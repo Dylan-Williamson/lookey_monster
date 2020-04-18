3820701e15a448d24563485d02854ae7cf8465be
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-18 17:29:25 +0000

    In progress

--- lib/api_manager.rb
+++ lib/api_manager.rb
@@ -1,3 +1,10 @@
-class APIManager
+class LookeyMonster::APIManager
   BASE_URL = "https://api.getsongbpm.com"
+  
+  def self.get_song_info
+    api_key = "4a45338f3b72c92981ea4c26c94ebf61"
+    url = BASE_URL + "/song/"
+    res = HTTParty.get(url, :headers => {
+      "X-Api-Key" => "#{api_key}"})
+  end
 end
