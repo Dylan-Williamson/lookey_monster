395cd1feea99867894d634a1b4f815dd82af1602
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-18 19:15:27 +0000

    updated gitignore

--- lib/api_manager.rb
+++ lib/api_manager.rb
@@ -2,9 +2,8 @@ class LookeyMonster::APIManager
   BASE_URL = "https://api.getsongbpm.com"
   
   def self.get_song_info
-    api_key = "4a45338f3b72c92981ea4c26c94ebf61"
+    bpm_api_key = "4a45338f3b72c92981ea4c26c94ebf61"
     url = BASE_URL + "/song/"
-    res = HTTParty.get(url, :headers => {
-      "X-Api-Key" => "#{api_key}"})
+    res = HTTParty.get(url, :headers => {"X-API-KEY" => "#{api_key}"})
   end
 end
