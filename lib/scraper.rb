7849d8d61d3fb15dda764f672a1a54cdd6b8d80e
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-19 23:10:05 +0000

    Corrects Scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,33 +3,24 @@ require 'httparty'
 require 'nokogiri'
 require_relative './lookey_monster'
 
-class LookeyMonster::Scraper
-  
-  # @@all = []
-  
-  def self.get_info(user_input)
-    user_input = gets.chomp
-    user_input = user_input.to_s
-    
-    url = "https://tunebat.com/Search?q=" + user_input.to_s.gsub!(" ","+")
-    html = HTTParty.get(url)
-    parsed_page = Nokogiri::HTML(html)
-    
-    songs = []
-    
-    song_listings = parsed_page.css('div.search-info-container')
-    
-    song_listings.each do |song_listing|
-      song = {
-        track: song_listing.css('div.row.search-track-name').text.scan(/\d+|\D+/),
-        artist: song_listing.css('div.row.search-artist-name').text.scan(/\d+|\D+/),
-        key: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[0],
-        tempo: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[3] + " BPM"
-      }
-      songs << song
-      @@all << songs
-    binding.pry
-    end
-    scraper
+def scraper
+  input = ""
+  input = gets.chomp
+  url = "https://tunebat.com/Search?q=" + input.gsub!(" ","+")
+  html = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(html)
+  songs = []
+  song_listings = parsed_page.css('div.search-info-container')
+  song_listings.each do |song_listing|
+    song = {
+      track: song_listing.css('div.row.search-track-name').text,
+      artist: song_listing.css('div.row.search-artist-name').text,
+      key: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[0],
+      tempo: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[3] + " BPM",
+    }
+    songs << song
+  binding.pry
   end
 end
+
+scraper
