f86a6b667375e6a7577911c6cd919b2e59fd1dbc
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-19 19:58:59 +0000

    completes scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,9 +4,12 @@ require 'nokogiri'
 
 require_relative './lookey_monster'
 def scraper
-  url = "https://tunebat.com/Search?q=hi"
+  input = ""
+  input = gets.chomp
+  url = "https://tunebat.com/Search?q=" + input.gsub!(" ","+")
   html = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(html)
+  songs = []
   song_listings = parsed_page.css('div.search-info-container')
   song_listings.each do |song_listing|
     song = {
@@ -15,6 +18,7 @@ def scraper
       key: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[0],
       tempo: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[3] + " BPM",
     }
+    songs << song
   binding.pry
   end
 end
