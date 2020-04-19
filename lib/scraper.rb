3d9fa084c66514fd76f2c21c821d47083615fb62
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-19 19:22:16 +0000

    Builds scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,20 @@
 require 'pry'
+require 'httparty'
+require 'nokogiri'
+
 require_relative './lookey_monster'
 def scraper
   url = "https://tunebat.com/Search?q=hi"
   html = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(html)
+  song_listings = parsed_page.css('div.search-info-container')
+  song_listings.each do |song_listing|
+    song = {
+      track: song_listing.css('div.row.search-track-name').text
+      artist: song_listing.css('div.row.search-artist-name').text
+      key: song_listing.css('div.row.search-attribute-value').first.text
+      tempo: song_listing.css('div.row.search-attribute-value')[1].text
+    }
   binding.pry 
 end
 
