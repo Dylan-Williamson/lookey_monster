1f0037801f9c26d2bbb5749f3d066fde27805850
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-20 14:50:26 +0000

    Updates to song class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,4 +1,4 @@
-require_relative './lookey_monster'
+# require_relative './lookey_monster'
 
 class LookeyMonster::CLI 
   
@@ -52,3 +52,78 @@ class LookeyMonster::CLI
 end
 
 LookeyMonster::CLI
+
+
+
+
+
+
+
+class LookeyMonster::CLI
+
+  def call
+    LookeyMonster::Scraper.new.make_top_songs
+    print logo
+    print "Welcome to LooKEY Monster!"
+    start
+  end
+
+  def start
+    print "\n\nWhich song would you like to see? [1-100]"
+    input = gets.strip.to_i
+
+    song = LookeyMonster::Song.find(input.to_i)
+
+    print_restaurant(restaurant)
+
+    puts ""
+    puts "Would you like to see another restaurant? Enter Y or N"
+
+    input = gets.strip.downcase
+    if input == "y"
+      start
+    elsif input == "n"
+      puts ""
+      puts "Thank you! Have a great day!"
+      exit
+    else
+      puts ""
+      puts "I don't understand that answer."
+      start
+    end
+  end
+
+  def print_restaurant(restaurant)
+    puts ""
+    puts "----------- #{restaurant.name} - #{restaurant.position} -----------"
+    puts ""
+    puts "#{restaurant.intro_quote}"
+    puts "Location:           #{restaurant.location}"
+    puts "Head Chef:          #{restaurant.head_chef}"
+    puts "Contact:            #{restaurant.contact}"
+    puts "Phone:             #{restaurant.phone}"
+    puts "Website:            #{restaurant.website_url}"
+
+    puts ""
+    puts "---------------Description--------------"
+    puts ""
+    puts "#{restaurant.description}"
+    puts ""
+
+    puts ""
+    puts "---------------About the Food--------------"
+    puts ""
+    puts "#{restaurant.food_style}"
+    puts ""
+  end
+
+  def print_restaurants(from_number)
+    puts ""
+    puts "---------- Restaurants #{from_number} - #{from_number+9} ----------"
+    puts ""
+    WorldsBestRestaurants::Restaurant.all[from_number-1, 10].each.with_index(from_number) do |restaurant, index|
+      puts "#{index}. #{restaurant.name} - #{restaurant.location}"
+    end
+  end
+
+end
