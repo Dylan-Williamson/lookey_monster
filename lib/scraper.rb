a863e90af570abb49c7b4c946064f1e4399e36c3
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-21 16:07:08 +0000

    Fixes scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,25 +7,50 @@ require_relative './song.rb'
 
 class LookeyMonster::Scraper
   
-  def top_scraper
-    url = "https://tunebat.com/"
-    html = HTTParty.get(url)
+  def songs
+    @@songs
   end
   
-  def search_scraper
+  def initialize
     @@songs = []
+  end
+  
+  # def top_scraper
+  #   url = "https://tunebat.com/"
+  #   html = HTTParty.get(url)
+  #   parsed_page = Nokogiri::HTML(html)
+  #   song_listings = parsed_page.css('div.search-info-container')
+  #   song_listings.each do |song_listing|
+  #     song = {
+  #       track: song_listing.css('div.row.search-track-name').text,
+  #       artist: song_listing.css('div.row.search-artist-name').text,
+  #       key: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[0],
+  #       tempo: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[3] + " BPM",
+  #     }
+  #     @@songs << song
+  #   binding.pry
+  #   end
+  # end
+  
+  def search_scraper
+    
     input = ""
     input = gets.chomp
-    url = "https://tunebat.com/Search?q=" + input.gsub!(" ","+")
+    if input.include?(" ") == true
+      url = "https://tunebat.com/Search?q=" + input.gsub!(" ","+")
+    else 
+      url = "https://tunebat.com/Search?q=" + input.downcase
+    end
     html = HTTParty.get(url)
     parsed_page = Nokogiri::HTML(html)
-    song_listings = parsed_page.css('div.search-info-container')
+    song_listings = parsed_page.css('div.searchResultNode')
     song_listings.each do |song_listing|
       song = {
         track: song_listing.css('div.row.search-track-name').text,
         artist: song_listing.css('div.row.search-artist-name').text,
         key: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[0],
         tempo: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[3] + " BPM",
+        url: song_listing.css('a.href').text
       }
       @@songs << song
     binding.pry
