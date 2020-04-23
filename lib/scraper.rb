5a23c0f89222b1e7b0ca4addc63ea11b89bdce92
Dylan Williamson <dirtyassweed@icloud.com>
2020-04-23 23:19:01 +0000

    Breaks scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,35 +14,35 @@ class LookeyMonster::Scraper
     @@songs
   end
   
-  def get_page
-    @@parsed_page = Nokogiri::HTML(open(@@url))
+  def self.get_page
+    parsed_page = Nokogiri::HTML(open(@@url))
   end
 
-  def scrape_songs
+  def self.scrape_songs
      self.get_page.css("div.searchResultNode.col-md-8.col-md-offset-2.col-sm-8.col-sm-offset-2.col-xs-12")
   end
   
-  def make_songs
+  def self.make_songs
     scrape_songs.each do |s|
       LookeyMonster::Song.new_from_index_page(s)
     end
   end
   
   
-  def scraper
+  def self.scraper
     LookeyMonster::Song.reset_all
-    @@input = gets.chomp
-    if @@input == "list" 
-      @@url = "https://tunebat.com/Index/GetFeaturedTracks"
-    elsif @@input.include?(" ") == true
-      @@url = "https://tunebat.com/Search?q=" + @@input.downcase.gsub!(" ","+")
+    input = gets.chomp
+    if input == "list" 
+      url = "https://tunebat.com/Index/GetFeaturedTracks"
+    elsif input.include?(" ") == true
+      url = "https://tunebat.com/Search?q=" + input.downcase.gsub!(" ","+")
     else 
-      @@url = "https://tunebat.com/Search?q=" + @@input.downcase
+      url = "https://tunebat.com/Search?q=" + input.downcase
     end
-    get_page
-    song_listings = @@parsed_page.css('div.searchResultList.row.main-row')
-    "https://tunebat.com/Index/GetFeaturedTracks"
-    binding.pry
+    LookeyMonster::Scraper.get_page
+    song_listings = parsed_page.css('div.searchResultList.row.main-row')
+    # "https://tunebat.com/Index/GetFeaturedTracks"
+    # binding.pry
     
     song_listings.each do |song_listing|
       song = {
@@ -50,7 +50,7 @@ class LookeyMonster::Scraper
         artist: song_listing.css('div.row.search-artist-name').text,
         key: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[0],
         tempo: song_listing.css('div.row.search-attribute-value').text.scan(/\d+|\D+/)[3] + "BPM",
-        url: song_listing.css('a["href"]').text
+        # url: song_listing.css(a["href"]).text
       }
       
       @@songs << song
